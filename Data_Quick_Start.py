47aac91e1153129dd1f1c3378b267f636166ef72
JiangP <jiangp17@mails.tsinghua.edu.cn>
2022-03-01 01:59:41 +0800

    liittle

--- Data_Quick_Start.py
+++ Data_Quick_Start.py
@@ -3,6 +3,9 @@ import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+a = np.array([1, 2, 3])
+print("a")
+
 
 from allensdk.brain_observatory.ecephys.ecephys_project_cache import EcephysProjectCache
 
